1481273b39db3e3c26669aed1ab155efc97f471c
松本尚裕 <matsumoto@hitsujistudio.com>
2020-06-22 13:53:21 +0900

    更新

--- takahiroid.com/news/index.php
+++ takahiroid.com/news/index.php
@@ -4,6 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="Keywords" content="まつもと,マツモト,タカヒロ,松本タカヒロ,タートルズ,ザ・タートルズ,turtles,餃子大王,sparky,まっちゃん" />
 <link rel="stylesheet" href="../style.css" type="text/css" media="screen,tv" />
+<link rel="stylesheet" href="../style_sp.css" type="text/css" media="screen,tv" />
 <script type="text/javascript" src="../js/minmax.js"></script>
 <script src="../share/js/rollover.js" type="text/javascript"></script>
 <title>TAKAHIROID.COM -松本タカヒロ- Home Page</title>
@@ -11,6 +12,13 @@
 <style type="text/css">
 .style3 {
   color: #000000
+}
+.alert{
+  font-size: 14px;
+  border:2px solid #e60617;
+  border-radius: 7px;
+  padding:1rem;
+  color: #000
 }
 </style>
 </head>
@@ -25,18 +33,75 @@ include("../menu.php");
   <div class="contents">
     <div class="news_left">
 
-<table width="500" border="0">
+<table width="500" border="0" class="news_table">
 
 
 <tr>
       <td>
-        <p class="cdtitle"><span>【LIVE TOUR】</span>ROLLY ＆ GlimRockers</p>
+        <p class="cdtitle"><span>【LIVE】</span>松酒屋（酒井ミキオ・松本タカヒロ・ha-j）</p>
+        <div class="txtBloc">
+
+          <p class="ttl">2020年7月12日(日) <a href="http://www.sactsow.com" target="_blank">新宿SACT! </a></p>
+          <p class="ttl">「松酒屋番外編　さらば新宿SACT! 〜愛の戦士たち〜」</p><br>
+          <p class="txt">
+          ●出演 : 松酒屋（【社長】 酒井ミキオ【副社長】松本タカヒロ【専務】ha-j）<br>
+          ●時間 : 開場 17：30 開演 18:00<br>
+          ●料金　前売り：¥3,900(別途1drink)<br>
+          ●チケット予約<br>
+
+【予約方法】SACT！にメールのみ(完全22名様限定)<br>
+【Mail】<a href="mailto:reservesow2009@sactsow.com" target="_blank">reservesow2009@sactsow.com</a>
+
+          </p>
+        </div>
+      </td>
+</tr>
+
+<tr>
+      <td>
+        <p class="cdtitle"><span>【LIVE】</span>ザ・タートルズ</p>
+        <div class="txtBloc">
+          <p class="ttl">2020年8月19日(水) 都内某所</a></p>
+          <p class="ttl">「松本タカヒロ50歳記念ライブー燃えるタカヒロナイト（仮称）」</p>
+          <p class="txt">
+詳細は7月に発表いたします。
+          </p>
+        </div>
+      </td>
+</tr>
+
+<tr>
+      <td>
+        <p class="cdtitle"><span>【LIVE TOUR】</span>ROLLY ＆ GlimRockers（公演延期）</p>
+
+<div class="alert">
+Rolly & Glimrockers7月公演開催について<br><br>
+
+メンバー・スタッフ・お店で開催の可否について検討を重ねてまいりましたが、各会場以下のように延期とさせていただきます。<br><br>
+
+▼西川口Hearts<br>
+7/18(土)→12/10(木)<br>
+※平日公演に変更ですので、開場開演時間を開場 18:15 開演 19:00　に変更いたします。<br><br>
+
+▼吉祥寺スターパインズカフェ　<br>
+7/19(日)→12/11(金)<br>
+※本公演は対バンでマルベリーズを予定しておりましたが出演無しとなりました。Rolly & Glimrockers単独での公演となります。<br><br>
+
+▼名古屋タイトロープ<br>
+7/21(火)→12/7(月)<br><br>
+
+▼大阪knave<br>
+7/22(水)→12/8(火)<br><br>
+
+詳細は、以下URLをご確認ください。<br>
+<a href="http://dragon.pump-annex.vivian.jp/?eid=1108570" target="_blank">http://dragon.pump-annex.vivian.jp/?eid=1108570</a>
+</div>
+
 
         <div class="txtBloc">
-<p class="ttl">【Rolly & Glimrockers LIVE 2020】</p>
 <br>
-
-<p class="ttl">7/18(土) 西川口 Hearts</a></p>
+<p class="ttl">【Rolly & Glimrockers LIVE 2020】</p>
+<p class="ttl">7/18(土) 西川口 Hearts</p>
 <p class="txt">
 ●時間 : 開場 16：30 開演 17:00<br>
 ●料金　前売り：¥4,400(別途1drink) 当日：¥4,900(別途1drink)<br>
@@ -67,16 +132,6 @@ include("../menu.php");
 ●料金　前売り：¥4,400(別途1drink) 当日：¥4,900(別途1drink)<br>
 ●チケット発売 : HPチケット予約
 </p>
-<br>
-
-<div class="txt" style="border: 1px solid #CCC;padding:1rem; border-radius: 5px;">
-<p>▼HPチケット予約</p><br>
-<p>お名前・枚数・お電話番号明記の上<br>
-<a href="mailto:team@glimrockers.com">team@glimrockers.com</a><br>
-までメールにて<br>
-2/8(土) 22：00〜予約開始
-</p>
-        </div>
       </td>
 </tr>
 
