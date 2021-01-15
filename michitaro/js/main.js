e311064c6c4e8ce74d1b5827571236020741076c
松本尚裕 <matsumoto@hitsujistudio.com>
2021-01-15 19:29:31 +0900

    no message

--- michitaro/js/main.js
+++ michitaro/js/main.js
@@ -11,7 +11,7 @@ function main() {
 
    /* ==============================================
   	Testimonial Slider
-  	=============================================== */
+  	=============================================== */ 
 
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -38,14 +38,14 @@ function main() {
         }
     });
 
-    $('body').scrollspy({
+    $('body').scrollspy({ 
         target: '.navbar-default',
         offset: 80
     })
 
   	$(document).ready(function() {
   	  $("#team").owlCarousel({
-
+  	 
   	      navigation : false, // Show next and prev buttons
   	      slideSpeed : 300,
   	      paginationSpeed : 400,
@@ -63,7 +63,7 @@ function main() {
   	  });
 
   	  $("#clients").owlCarousel({
-
+  	 
   	      navigation : false, // Show next and prev buttons
   	      slideSpeed : 300,
   	      paginationSpeed : 400,
