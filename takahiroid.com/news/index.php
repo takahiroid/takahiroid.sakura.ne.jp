6639bb3f9be522c0139ac44391abfb79a768d05d
松本尚裕 <matsumoto@hitsujistudio.com>
2023-12-30 12:04:16 +0900

    no message

--- takahiroid.com/news/index.php
+++ takahiroid.com/news/index.php
@@ -1,174 +1,146 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="Keywords" content="まつもと,マツモト,タカヒロ,松本タカヒロ,タートルズ,ザ・タートルズ,turtles,餃子大王,sparky,まっちゃん" />
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script type="text/javascript" src="/common/js/common.js"></script>
-<link rel="stylesheet" href="/common/css/style.css" type="text/css"/>
-<link rel="stylesheet" href="/common/css/style_sp.css" type="text/css" />
-<script src="../common/js/rollover.js" type="text/javascript"></script>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="Keywords" content="まつもと,マツモト,タカヒロ,松本タカヒロ,タートルズ,ザ・タートルズ,turtles,餃子大王,sparky,まっちゃん" />
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="/common/js/common.js"></script>
+  <link rel="stylesheet" href="/common/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="/common/css/style_sp.css" type="text/css" />
+  <script src="../common/js/rollover.js" type="text/javascript"></script>
 
 
-<!-- PAGE TOPに戻るボタン -->
-<script type="text/javascript" src="../common/js/back_top.js"></script>
+  <!-- PAGE TOPに戻るボタン -->
+  <script type="text/javascript" src="../common/js/back_top.js"></script>
 
-<title>TAKAHIROID.COM -松本タカヒロ- Home Page</title>
-<style type="text/css">
-.alert{
-  font-size: 16px;
-  border:2px solid #ff0000;
-  border-radius: 4px;
-  padding:1rem;
-  width: 98%;
-  margin:0 auto;
-  box-sizing: border-box;
-}
-</style>
+  <title>TAKAHIROID.COM -松本タカヒロ- Home Page</title>
+  <style type="text/css">
+    .alert {
+      font-size: 16px;
+      border: 2px solid #ff0000;
+      border-radius: 4px;
+      padding: 1rem;
+      width: 98%;
+      margin: 0 auto;
+      box-sizing: border-box;
+    }
+  </style>
 </head>
 
 <body id="news">
-<div class="wrapper">
-<?php include("../common/inc/header.php"); ?>
+  <div class="wrapper">
+    <?php include("../common/inc/header.php"); ?>
 
-  <div class="sp_title sp"><p>Live&Media</p></div>
-
-  <div class="contents">
-    <div class="wrap_area">
-      <div class="news_left">
-
-<table border="0" class="news_table">
-
-
-<tr>
-  <td>
-    <p class="cdtitle">
-      <span style="background-color:#70b539">LIVE</span>
-      <br class="sp">餃子大王
-    </p>
-    <div class="txtBloc">
-      <p class="ttl"><a href="https://clubque.net/" target="_blank" class="bold">2023/07/16 (Sun)　東京・下北沢 CLUB Que</a></p>
-      <p class="ttl bold" style="margin-top: 0.5rem">“おかげサマーで40周年 餃子大王夏ワンマン”<br>餃子大王 -oneman-</p>
-      <p class="txt">
-
-■開場 12:00／開演 12:30 <br>
-■料金 前売 2,000円／当日 2,300円（1D別）<br>
-■チケット：Que店頭 3/25 16:00～ , イープラス 4/1 10:00～<br>
+    <div class="sp_title sp">
+      <p>Live&Media</p>
     </div>
-  </td>
-</tr>
 
-<tr>
-  <td>
-    <p class="cdtitle">
-      <span style="background-color:#70b539">LIVE</span>
-      <br class="sp">枕本バンド
-    </p>
-    <div class="txtBloc">
-      <p class="ttl"><a href="http://www.chelseahotel.jp/" target="_blank" class="bold">2023.08.26(土)　東京・渋谷CHELSEA HOTEL</a></p>
-      <p class="ttl bold" style="margin-top: 0.5rem">”スキップカウズ presents イマフェス★オリンピックPURE”</p>
-      <p class="txt">
-■出演：スキップカウズ / ダイナマイト☆ナオキ / ザ・キャプテンズ / 枕本トクロウ / THE まっすぐ(大森洋平×染谷俊×野崎真助) / ニーネ / Magical Sixx / Empty Black Box / 逆EDGE
-■開場 13:00／開演 13:30 <br>
-■料金 前売 3,500円／当日 4,000円（1D別）<br>
-■チケット：<br>
-<a href="https://eplus.jp/sf/detail/3829220001-P0030001" target="_blank">https://eplus.jp/sf/detail/3829220001-P0030001</a><br>
-<a href="https://t.livepocket.jp/e/eyl2c" target="_blank">https://t.livepocket.jp/e/eyl2c</a><br>
-    </div>
-  </td>
-</tr>
+    <div class="contents">
+      <div class="wrap_area">
+        <div class="news_left">
 
-<tr>
-  <td>
-    <p class="cdtitle">
-      <span style="background-color:#70b539">LIVE</span>
-      <br class="sp">モダンチョキチョキズ / 餃子大王
-    </p>
-    <div class="txtBloc">
-      <p class="ttl"><a href="https://shibuya-o.com/west/" target="_blank" class="bold">2023/9/10(日)　Ｓｐｏｔｉｆｙ　Ｏ－ＷＥＳＴ (東京都)</a></p>
-      <p class="ttl bold" style="margin-top: 0.5rem">よせばいいのに　～　モダチョキｖｓ餃子大王　東京編</p>
-      <p class="txt">
-■出演：モダンチョキチョキズ / 餃子大王<br>
-■開場14:30／開演 15:00 <br>
-■料金 1Fスタンディング¥5,400　2F自由席¥5,400（1D別）<br>
-■チケット：<br>
-チケットぴあ (Pコード:244-944)<br>
-ローソンチケット(Lコード:70470)<br>
-イープラス<br>
-CNプレイガイド　0570-08-9999<br>
-    </div>
-  </td>
-</tr>
+          <table border="0" class="news_table">
 
-<tr>
-  <td>
-    <p class="cdtitle">
-      <span style="background-color:#70b539">LIVE</span>
-      <br class="sp">世良公則 x JETROX
-    </p>
-    <div class="txtBloc">
-      <p class="ttl"><a href="http://www.namba-hatch.com/index.php" target="_blank" class="bold">2023/10/08(日)　なんばHatch (大阪)</a></p>
-      <p class="ttl bold" style="margin-top: 0.5rem">JETROX LIVE2023</p>
-      <p class="txt">
-■開場16:00／開演17:00 <br>
-■料金 8,800円 (税込・入場時別途ドリンク代必要) ※6歳未満入場不可<br>
-■チケット：2023年8月26日（土曜日）10:00より各プレイガイドにて<br>
-■お問合せ：キョードーインフォメイション【電話】0570-200-888（月-土11:00～16:00）<br>
-    </div>
-  </td>
-</tr>
+            <tr>
+              <td>
+                <p class="cdtitle">
+                  <span style="background-color:#70b539">LIVE・GUITAR・ソロ</span>
+                  <br class="sp">NOB FROM CURIOではギター、そしてソロ出演も決定！
+                </p>
+                <div class="txtBloc">
+                  <p class="ttl"><a href="https://www.banana-hall.com/" target="_blank" class="bold">2023/11/18(土)　梅田ＢａｎａｎａＨａｌｌ (大阪府)</a></p>
+                  <p class="ttl bold" style="margin-top: 0.5rem">ロック道連れフェスティバル</p>
+                  <p class="txt">
+                    ■出演：太陽の塔 / ドミンゴス / スキップカウズ / プロペラ / ザ・カスタネッツ / ＮＯＢ / 森山公一 / 松本タカヒロ<br>
+                    ■開場13:00／開演14:30 <br>
+                    ■料金 ¥5,500（ドリンク代別）<br>
+                    ■チケット：発売中　各プレイガイドにて<br><br>
 
-<tr>
-      <td>
-        <p class="cdtitle"><span style="background-color:#70b539">SHOP・GOODS</span><br>ヴィレッジヴァンガードの新型ショッピングプラットホーム「voon」にて「松本タカヒロ」書き下ろしキャラのセレクトショップがスタートしました</p>
-        <div class="txtBloc">
-          <p class="ttl"><a href="https://voon.shop/users/12896fc9-3ed3-44b8-85db-71d6c5e267df" target="_blank" class="bold">Village Vangurd 「voon」</a></p>
-          <p class="ttl bold" style="margin-top: 0.5rem"></p>
-          <p class="txt">
-以前にSNSで公開した落書きキャラ。なんと！<br>
-ヴィレッジヴァンガードさんから、商品化のオファーを頂きました。<br>
-<br>
-ヴィレヴァンの新型ショッピングプラットホーム「voon」にて<br>
-松本タカヒロオリジナルキャラクターのセレクトショップがスタートしました<br>
-今回の為に描き下ろしました、是非ヴィレヴァンでチェックしてねー
-<br><br>
-サイトはこちら<br>
-<a href="https://voon.shop/users/12896fc9-3ed3-44b8-85db-71d6c5e267df" target="_blank">Village Vangurd 「voon」</a>
-<br>
+                    ＊NOB FROM CURIOでのギター、そしてサブステージでのソロ出演も決定！<br><br>
+                    <img src="./img/20231118.jpg" class="newsimg">
+                  </p>
+                </div>
+              </td>
+            </tr>
 
-          </p>
-          <div style="width: 100%;"><img src="img/voon.jpeg" style="width: 100%;"></div>
+            <tr>
+              <td>
+                <p class="cdtitle">
+                  <span style="background-color:#70b539">LIVE</span>
+                  <br class="sp">松酒屋
+                </p>
+                <div class="txtBloc">
+                  <p class="ttl"><a href="https://www.banana-hall.com/" target="_blank" class="bold">2023/12/9(土)　新宿Sact!</a></p>
+                  <p class="ttl bold" style="margin-top: 0.5rem">松酒屋 ～十二幕～「師走の松酒屋」</p>
+                  <p class="txt">
+                    ■出演：酒井ミキオ(社長)・松本タカヒロ(副社長)・ha-j(専務)<br>
+                    ■開場18:00／開演18:30 <br>
+                    ■料金 前売：¥4,000+1drink代600円別・当日 / ¥4,500+1drink代600円別<br>
+                    ■チケット：ご予約はコチラから→<a href="https://tiget.net/events/273037" target="_blank">TIGET</a><br>
+                    ■配信チケット：<a href="https://twitcasting.tv/akyhiroshi/shopcart/267721" target="_blank">ツイキャス</a><br>
+                    ■お問い合わせ：infosow@sactco.jp<br><br>
+
+                    ＊チケット予約のお支払いは当日、入場時に受付にてお願い致します。<br>
+                    ＊入場は予約の番号順の入場となります。<br>
+                    ＊開場15分前整列。開場後は来場順の入場となります。<br>
+                  </p>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <p class="cdtitle"><span style="background-color:#70b539">SHOP・GOODS</span><br>ヴィレッジヴァンガードの新型ショッピングプラットホーム「voon」にて「松本タカヒロ」書き下ろしキャラのセレクトショップがスタートしました</p>
+                <div class="txtBloc">
+                  <p class="ttl"><a href="https://voon.shop/users/12896fc9-3ed3-44b8-85db-71d6c5e267df" target="_blank" class="bold">Village Vangurd 「voon」</a></p>
+                  <p class="ttl bold" style="margin-top: 0.5rem"></p>
+                  <p class="txt">
+                    以前にSNSで公開した落書きキャラ。なんと！<br>
+                    ヴィレッジヴァンガードさんから、商品化のオファーを頂きました。<br>
+                    <br>
+                    ヴィレヴァンの新型ショッピングプラットホーム「voon」にて<br>
+                    松本タカヒロオリジナルキャラクターのセレクトショップがスタートしました<br>
+                    今回の為に描き下ろしました、是非ヴィレヴァンでチェックしてねー
+                    <br><br>
+                    サイトはこちら<br>
+                    <a href="https://voon.shop/users/12896fc9-3ed3-44b8-85db-71d6c5e267df" target="_blank">Village Vangurd 「voon」</a>
+                    <br>
+
+                  </p>
+                  <div style="width: 100%;"><img src="img/voon.jpeg" style="width: 100%;"></div>
+                </div>
+              </td>
+            </tr>
+
+
+
+          </table>
+          <a href="past_2023.php">→過去のライブ - 2023年</a><br>
+          <a href="past_2022.php">→過去のライブ - 2022年</a><br>
+          <a href="past_2021.php">→過去のライブ - 2021年</a><br>
+          <a href="past_2020.php">→過去のライブ - 2020年</a><br>
+          <a href="past_2019.php">→過去のライブ - 2019年</a><br>
+          <a href="past_2018.php">→過去のライブ - 2018年</a><br>
+          <a href="past_2017.php">→過去のライブ - 2017年</a><br>
+          <a href="past_2016.php">→過去のライブ - 2016年</a>
+
         </div>
-      </td>
-</tr>
 
-
-
-</table>
-<a href="past_2023.php">→過去のライブ - 2023年</a><br>
-<a href="past_2022.php">→過去のライブ - 2022年</a><br>
-<a href="past_2021.php">→過去のライブ - 2021年</a><br>
-<a href="past_2020.php">→過去のライブ - 2020年</a><br>
-<a href="past_2019.php">→過去のライブ - 2019年</a><br>
-<a href="past_2018.php">→過去のライブ - 2018年</a><br>
-<a href="past_2017.php">→過去のライブ - 2017年</a><br>
-<a href="past_2016.php">→過去のライブ - 2016年</a>
-
-      </div>
-
-      <div class="news_right pc">
-        <img src="img/news.gif" alt="NEWS" class="page_ttl" /><br />
-        <div class="spaceman">
-          <img src="/common/img/spaceman.gif">
+        <div class="news_right pc">
+          <img src="img/news.gif" alt="NEWS" class="page_ttl" /><br />
+          <div class="spaceman">
+            <img src="/common/img/spaceman.gif">
+          </div>
         </div>
       </div>
+
     </div>
+    <?php include("../common/inc/footer.php"); ?>
 
   </div>
-  <?php include("../common/inc/footer.php"); ?>
-
   </div>
-</div>
 </body>
+
 </html>
--- takahiroid.com/news/past_2023.php
+++ takahiroid.com/news/past_2023.php
@@ -1,76 +1,126 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="Keywords" content="まつもと,マツモト,タカヒロ,松本タカヒロ,タートルズ,ザ・タートルズ,turtles,餃子大王,sparky,まっちゃん" />
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script type="text/javascript" src="/common/js/common.js"></script>
-<link rel="stylesheet" href="/common/css/style.css" type="text/css"/>
-<link rel="stylesheet" href="/common/css/style_sp.css" type="text/css" />
-<script src="../common/js/rollover.js" type="text/javascript"></script>
-<!-- PAGE TOPに戻るボタン -->
-<script type="text/javascript" src="../common/js/back_top.js"></script>
-<title>TAKAHIROID.COM -松本タカヒロ- Home Page</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="Keywords" content="まつもと,マツモト,タカヒロ,松本タカヒロ,タートルズ,ザ・タートルズ,turtles,餃子大王,sparky,まっちゃん" />
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="/common/js/common.js"></script>
+  <link rel="stylesheet" href="/common/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="/common/css/style_sp.css" type="text/css" />
+  <script src="../common/js/rollover.js" type="text/javascript"></script>
+  <!-- PAGE TOPに戻るボタン -->
+  <script type="text/javascript" src="../common/js/back_top.js"></script>
+  <title>TAKAHIROID.COM -松本タカヒロ- Home Page</title>
 </head>
 
 <body id="news">
-<div class="wrapper">
-<?php include("../common/inc/header.php"); ?>
-<div class="sp_title sp"><p>Live</p></div>
-
-  <div class="contents">
-    <div class="wrap_area">
-      <div class="news_left">
-
-        <div class="txtBack sp"><a href="/news">←BACK</a></div>
-        <h4 class="ttl_year">-- 2023 LIVE --</h4><br>
-
-
-<div class="past_live_txt">
-<div class="boxPastLive">
-  <strong>餃子大王</strong><br>
-2023年1月8日（日）下北沢 CLUB Que<br>「40周年プレシーズン～餃子はまもなく40歳～餃子大王東京ワンマン」
-</div>
-</div>
-
-<div class="past_live_txt">
-<div class="boxPastLive">
-<strong>Glimrockers （小畑ポンプ、松本タカヒロ、永井ルイ）</strong><br>
-2023年2月24日（金）東京 荻窪　Top Beat Club<br>The Grand Year Of Top Beat ~Cum On Feel The Roll＃1~」<br>
-出演：The HIGH / ザ・キャプテンズ / グリムロッカーズ
-</div>
-</div>
-
-<div class="past_live_txt">
-<div class="boxPastLive">
-<strong>餃子大王</strong><br>
-2023年3月19日（日）心斎橋ＪＡＮＵＳ (大阪府) <br>「よせばいいのに　～　モダチョキｖｓ餃子大王」
-</div>
-</div>
-
-<div class="past_live_txt">
-<div class="boxPastLive">
-<strong>松酒屋</strong><br>
-2023年03月25日(土) 新宿Sact<br>
-『松酒屋〜十一幕〜』〜春一番〜<br>
-出演：酒井ミキオ(社長)・松本タカヒロ(副社長)・ha-j(専務)
-</div>
-</div>
-	   </div>
-
-      <div class="news_right">
-        <img src="img/news.gif" alt="NEWS" class="page_ttl" /><br />
-        <div class="spaceman">
-          <img src="/common/img/spaceman.gif">
-        </div>
-      </div>
+  <div class="wrapper">
+    <?php include("../common/inc/header.php"); ?>
+    <div class="sp_title sp">
+      <p>Live</p>
     </div>
 
-  </div>
-  <?php include("../common/inc/footer.php"); ?>
+    <div class="contents">
+      <div class="wrap_area">
+        <div class="news_left">
+
+          <div class="txtBack sp"><a href="/news">←BACK</a></div>
+          <h4 class="ttl_year">-- 2023 LIVE --</h4><br>
+
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>餃子大王</strong><br>
+              2023年1月8日（日）下北沢 CLUB Que<br>「40周年プレシーズン～餃子はまもなく40歳～餃子大王東京ワンマン」
+            </div>
+          </div>
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>Glimrockers （小畑ポンプ、松本タカヒロ、永井ルイ）</strong><br>
+              2023年2月24日（金）東京 荻窪　Top Beat Club<br>The Grand Year Of Top Beat ~Cum On Feel The Roll＃1~」<br>
+              出演：The HIGH / ザ・キャプテンズ / グリムロッカーズ
+            </div>
+          </div>
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>餃子大王</strong><br>
+              2023年3月19日（日）心斎橋ＪＡＮＵＳ (大阪府) <br>「よせばいいのに　～　モダチョキｖｓ餃子大王」
+            </div>
+          </div>
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>松酒屋</strong><br>
+              2023年03月25日(土) 新宿Sact<br>
+              『松酒屋〜十一幕〜』〜春一番〜<br>
+              出演：酒井ミキオ(社長)・松本タカヒロ(副社長)・ha-j(専務)
+            </div>
+          </div>
+
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>餃子大王</strong><br>
+              2023/07/16 (Sun)　東京・下北沢 CLUB Que<br>
+              おかげサマーで40周年 餃子大王夏ワンマン<br>
+            </div>
+          </div>
+
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>枕本バンド</strong><br>
+              2023.08.26(土)　東京・渋谷CHELSEA HOTEL<br>
+              スキップカウズ presents イマフェス★オリンピックPURE<br>
+              出演：スキップカウズ / ダイナマイト☆ナオキ / ザ・キャプテンズ / 枕本トクロウ / THE まっすぐ(大森洋平×染谷俊×野崎真助) / ニーネ / Magical Sixx / Empty Black Box / 逆EDGE
+            </div>
+          </div>
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>餃子大王</strong><br>
+              2023/9/10(日)　Spotify　O-WEST (東京都)<br>
+              よせばいいのに　～　モダチョキｖｓ餃子大王　東京編）<br>
+              出演：モダンチョキチョキズ / 餃子大王
+            </div>
+          </div>
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>世良公則 x JETROX</strong><br>
+              2023/10/08(日)　なんばHatch (大阪)<br>
+              JETROX LIVE2023<br>
+            </div>
+          </div>
+
+          <div class="past_live_txt">
+            <div class="boxPastLive">
+              <strong>NOB FROM CURIO</strong><br>
+              2023/10/28(土)　下北沢251（東京）<br>
+              NOB FROm CURIO 50th ANIVERSARY NIGHT）<br>
+              出演：スキップカウズ / SHOW-SKA / NOB FROM CURIO
+            </div>
+          </div>
+
+        </div>
+
+        <div class="news_right">
+          <img src="img/news.gif" alt="NEWS" class="page_ttl" /><br />
+          <div class="spaceman">
+            <img src="/common/img/spaceman.gif">
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <?php include("../common/inc/footer.php"); ?>
 
   </div>
-</div>
+  </div>
 </body>
+
 </html>
