8c31c42ba131ed66c554c96dc9873773011350d2
松本尚裕 <matsumoto@hitsuji.ne.jp>
2026-02-09 18:25:17 +0900

    no message

--- takahiroid.com/common/js/common.js
+++ takahiroid.com/common/js/common.js
@@ -2,15 +2,11 @@ $(function() {
   //スマホ用ナビ
   $('.menu-trigger').on('click', function() {
     $(this).toggleClass("active");
-    if($('.gnavi_sp .menu').css('display') === 'block') {
-      $('.gnavi_sp .menu').slideUp('1500');
-    }else {
-      $('.gnavi_sp .menu').slideDown('1500');
-    }
+    $('.gnavi_sp .menu').toggleClass("open");
   });
 
-  $('.menu-item a').on('click', function() {
+  $('.gnavi_sp .menu a').on('click', function() {
     $('.menu-trigger').removeClass("active");
-    $('.gnavi_sp .menu').slideUp('1500');
+    $('.gnavi_sp .menu').removeClass("open");
   });
 });
